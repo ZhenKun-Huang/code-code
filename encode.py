701a2e86ed742a54d5ddb611f001c7be005c3608
ZhenKun-Huang <79698221+ZhenKun-Huang@users.noreply.github.com>
2024-10-05 17:02:30 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -5,12 +5,12 @@ str=str(enco,'utf-8')
 l1=(datetime.datetime.now().hour)%0x5
 l2=(datetime.datetime.now().day)%0x6
 l3=(datetime.datetime.now().month)%0x7
-l4=(datetime.datetime.now().minute)%0x8
+
 l5=(datetime.datetime.now().year)%0x9
 str=str.replace("5",f"{l1}")
 str=str.replace("6",f"{l2}")
 str=str.replace("7",f"{l3}")
-str=str.replace("8",f"{l4}")
+
 str=str.replace("9",f"{l5}")
 by=bytes(str,encoding='utf-8')
 print(by.decode('unicode_escape'))
