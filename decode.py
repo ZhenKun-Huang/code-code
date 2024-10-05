06b1937fbd2999d3dc7bb0b4409d460f98e4bdd4
ZhenKun-Huang <79698221+ZhenKun-Huang@users.noreply.github.com>
2024-10-05 17:00:03 +0800

    Create decode.py

---
+++ decode.py
@@ -0,0 +1,16 @@
+import datetime
+str2=""
+enco=str2.encode('unicode_escape')
+str=str(enco,'utf-8')
+l1=(datetime.datetime.now().hour)%0xff
+l2=(datetime.datetime.now().day)%0xff
+l3=(datetime.datetime.now().month)%0xff
+l4=(datetime.datetime.now().minute)%0xff
+l5=(datetime.datetime.now().year)%0xff
+str=str.replace(f"{l5}","9")
+str=str.replace(f"{l4}","8")
+str=str.replace(f"{l3}","7")
+str=str.replace(f"{l2}","6")
+str=str.replace(f"{l1}","5")
+by=bytes(str,encoding='utf-8')
+print(by.decode('unicode_escape'))
