1bbf78100bd9aa910a9ae225fde808f2ed8aeb93
ZhenKun-Huang <79698221+ZhenKun-Huang@users.noreply.github.com>
2024-10-05 17:11:08 +0800

    Update decode.py

--- decode.py
+++ decode.py
@@ -2,11 +2,11 @@ import datetime
 str2=""
 enco=str2.encode('unicode_escape')
 str=str(enco,'utf-8')
-l1=(datetime.datetime.now().hour)%0xff
-l2=(datetime.datetime.now().day)%0xff
-l3=(datetime.datetime.now().month)%0xff
+l1=(datetime.datetime.now().hour)%0xf
+l2=(datetime.datetime.now().day)%0xf
+l3=(datetime.datetime.now().month)%0xf
 
-l5=(datetime.datetime.now().year)%0xff
+l5=(datetime.datetime.now().year)%0xf
 str=str.replace(f"{l5}","9")
 
 str=str.replace(f"{l3}","7")
