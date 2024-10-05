1f434f7c7f43303c6a4ef844f411455476579253
ZhenKun-Huang <79698221+ZhenKun-Huang@users.noreply.github.com>
2024-10-05 17:11:26 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -2,11 +2,11 @@ import datetime
 str2=""
 enco=str2.encode('unicode_escape')
 str=str(enco,'utf-8')
-l1=(datetime.datetime.now().hour)%0x5
-l2=(datetime.datetime.now().day)%0x6
-l3=(datetime.datetime.now().month)%0x7
+l1=(datetime.datetime.now().hour)%0xf
+l2=(datetime.datetime.now().day)%0xf
+l3=(datetime.datetime.now().month)%0xf
 
-l5=(datetime.datetime.now().year)%0x9
+l5=(datetime.datetime.now().year)%0xf
 str=str.replace("5",f"{l1}")
 str=str.replace("6",f"{l2}")
 str=str.replace("7",f"{l3}")
